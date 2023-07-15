e9df37dacb24758b7729b97caf937350fa9effa0
Sushil Kumar <skumar18@wayfair.com>
2023-07-15 21:29:09 +0530

    Setting delay from 100 ms to 1 sec

--- consumer/src/main/java/xyz/sushil/kedademo/KafkaConsumer.java
+++ consumer/src/main/java/xyz/sushil/kedademo/KafkaConsumer.java
@@ -8,7 +8,7 @@ public class KafkaConsumer {
 
   @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.consumer-group-id}")
   public void handle(String message) throws InterruptedException {
-    int delay = Integer.parseInt(message);
+    int delay = Integer.parseInt(message) * 100;
     System.out.println("Sleeping for : " + delay + " ms.");
     Thread.sleep(delay);
   }
